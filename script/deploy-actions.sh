4bac0ad94f3b3a6198e02839fbf2b644abd621e6
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2020-02-23 00:00:42 -0500

    propagate version numbers for release

--- script/deploy-actions.sh
+++ script/deploy-actions.sh
@@ -5,19 +5,19 @@
 
 set -e
 
-if [[ "$GITHUB_ACTIONS" != 'true' ]];
+if [[ "$GITHUB_ACTIONS" != 'true' ]]; then
   echo "Only call this script in GitHub Actions"
   exit 1
 fi 
 
-if [[ "$GITHUB_REPOSITORY" != 'typedclojure/typedclojure' ]];
+if [[ "$GITHUB_REPOSITORY" != 'typedclojure/typedclojure' ]]; then
   echo "This script only deploys in typedclojure/typedclojure, not $GITHUB_REPOSITORY. Doing nothing."
   exit 0
 fi
 
 COMMIT_MSG=$(git log --format=%B -n 1 "${GITHUB_SHA}")
-
-if [[ "$COMMIT_MSG" == "[typedclojure-release]*" ]]; then
+echo $COMMIT_MSG
+if [[ "$COMMIT_MSG" =~ ^\[typedclojure-release\] ]]; then
   #https://stackoverflow.com/a/9294015
   COMMIT_MSG_ARRAY=($COMMIT_MSG)
   ./script/release-and-push.sh "${COMMIT_MSG_ARRAY[1]}" "${COMMIT_MSG_ARRAY[2]}"
--- script/release-and-push.sh
+++ script/release-and-push.sh
@@ -41,12 +41,12 @@ git config --local user.name "Ambrose Bonnaire-Sergeant"
 RELEASE_VERSION=$1
 DEVELOPMENT_VERSION=$2
 
-if [[ -z "$RELEASE_VERSION" ]];
+if [[ -z "$RELEASE_VERSION" ]]; then
   echo "Must specify release version"
   exit 1
 fi
 
-if [[ -z "$DEVELOPMENT_VERSION" ]];
+if [[ -z "$DEVELOPMENT_VERSION" ]]; then
   echo "Must specify development version"
   exit 1
 fi
@@ -56,14 +56,19 @@ fi
 # pushing back to master, but there's a chance the version
 # was partially deployed. The correct fix (wrt clojars) is to simply
 # deploy a new version (eg., if 1.0.0 fails, try 1.0.1 next).
+( set -x;
 mvn release:prepare release:perform \
   -DreleaseVersion="$RELEASE_VERSION" \
   -Dtag="$RELEASE_VERSION" \
   -DdevelopmentVersion="$DEVELOPMENT_VERSION"
+  )
 
+( set -x;
 ./script/bump-readme-version "$RELEASE_VERSION"
+)
 
 git add .
 git commit -m "Bump README versions for $RELEASE_VERSION"
 
+# DON'T PRINT HERE
 git push "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" master --ff-only --tags
