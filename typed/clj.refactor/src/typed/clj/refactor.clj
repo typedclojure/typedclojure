d1e7ed46cb7ceada975510a32c2db6fc48da77eb
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2020-11-19 00:00:00 -0500

    work on smarter destructuring expansion

--- typed/clj.refactor/src/typed/clj/refactor.clj
+++ typed/clj.refactor/src/typed/clj/refactor.clj
@@ -1,5 +1,6 @@
 (ns typed.clj.refactor
   "Alpha"
+  (:refer-clojure :exclude [run!])
   (:require [typed.clj.analyzer :as ana]
             [typed.cljc.analyzer :as ana-cljc]
             [typed.cljc.analyzer.ast :as ast]
@@ -57,11 +58,81 @@
   (-> expr
       (ast/update-children #(refactor-form* % rdr-ast opt))))
 
+(defmulti -refactor-special (fn [op-sym expr rdr-ast opt] op-sym))
+
+(defn ^:private run!
+  "Runs the supplied procedure (via reduce), for purposes of side
+  effects, on successive items in the collection. Returns nil"
+  {:added "1.7"}
+  [proc coll]
+  (reduce #(do (proc %2) nil) nil coll)
+  nil)
+
+(defn record-destructuring [b file-map-atom]
+  (cond
+    (vector? b) (do
+                  (when (seq (meta b))
+                    (swap! file-map-atom assoc
+                           (select-keys (meta b)
+                                        [:line :column
+                                         :end-line :end-column
+                                         :file])
+                           [:vector-destructure b]))
+                  (run! #(destructure-keys->explicit % file-map-atom) b))
+    (map? b) (do
+               (when (seq (meta b))
+                 (swap! file-map-atom assoc
+                        (select-keys (meta b)
+                                     [:line :column
+                                      :end-line :end-column
+                                      :file])
+                        [:map-destructure b]))
+               (->> b
+                    (run! (fn [[bi v :as orig]]
+                            (cond
+                              (#{:as} bi) [orig]
+                              (and (#{:keys} bi)
+                                   (vector? v))
+                              (run! (fn [bk]
+                                      (when (seq (meta bk))
+                                        (swap! file-map-atom assoc
+                                               (select-keys (meta bk)
+                                                            [:line :column
+                                                             :end-line :end-column
+                                                             :file])
+                                               [:map-destructure-keys-enty bk])))
+                                    v)
+                              ;;TODO namespaced keywords, :syms :strs
+                              (keyword? bi) nil
+                              :else nil)))))))
+
+(comment
+  (let [a (atom {})]
+    (record-destructuring
+      '{:keys [a b]}
+      a)
+    @a)
+  )
+
+(defmethod -refactor-special 'clojure.core/let
+  [_ {:keys [form] :as expr} rdr-ast {:keys [file-map-atom] :as opt}]
+  (let [bindings (second form)]
+    (when (and (vector? bindings)
+               (even? (count bindings)))
+      (let [_ (doseq [[b _] (partition 2 bindings)]
+                (record-destructuring b file-map-atom))]
+        (-> expr
+            ana-cljc/analyze-outer
+            (refactor-form* rdr-ast opt))))))
+
+(defmethod -refactor-special :default [_ _ _ _] nil)
+
 (defn- refactor-form* [expr rdr-ast {:keys [file-map-atom] :as opt}]
   (let [refactor-form* (fn [expr]
                          (case (::ana-cljc/op expr)
                            ::ana-cljc/unanalyzed (let [{:keys [form env]} expr
                                                        op-sym (ana/resolve-op-sym form env)]
+                                                   (prn op-sym)
                                                    (when op-sym
                                                      (let [mta (meta (first form))]
                                                        (when ((every-pred :line :column) mta)
@@ -70,7 +141,8 @@
                                                                                   :end-line :end-column
                                                                                   :file])
                                                                 [:var op-sym]))))
-                                                   (recur (ana-cljc/analyze-outer expr)))
+                                                   (or (some-> op-sym (-refactor-special expr rdr-ast opt))
+                                                       (recur (ana-cljc/analyze-outer expr))))
                            (-> expr
                                ana-cljc/run-pre-passes
                                (-refactor-form* rdr-ast opt)
@@ -117,6 +189,8 @@
                       LineNumberingPushbackReader.
                       (indexing-push-back-reader 1 "example.clj"))]
     (let [{form :val :as rdr-ast} (rdr/read+ast rdr)
+          opt (cond-> opt
+                (not (:file-map-atom opt)) (assoc :file-map-atom (atom {})))
           fm (file-map form rdr-ast opt)
           fq-string (rdr/ast->string (fq-rdr-ast rdr-ast fm {}))]
       fq-string)))
@@ -130,9 +204,18 @@
   (refactor-form-string "(fn [])" {})
   (refactor-form-string "(fn [a] (a))" {})
   (refactor-form-string "(fn [a] (+ a 3))" {})
+
+  (refactor-form-string "+" {})
+  ;=> "clojure.core/+"
   (refactor-form-string "(defn foo [a] (+ a 3))" {})
+  ;=> "(clojure.core/defn foo [a__#0] (clojure.core/+ a__#0 3))"
   (refactor-form-string "(defmacro foo [a] `(+ ~a 3))" {})
+  ;=> "(clojure.core/defmacro foo [a__#0] `(+ ~a__#0 3))"
   (refactor-form-string "(defmacro foo [a] `(+ ~@a 3))" {})
   (refactor-form-string "(str (fn []))" {})
   (refactor-form-string "`fn" {})
+  (let [a (atom {})]
+    (refactor-form-string "(fn [{:keys [a]}] a)"
+                          {:file-map-atom a})
+    @a)
   )
