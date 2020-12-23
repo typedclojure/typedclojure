03dba4d07c730fe6801c6e51ca1ef97ddf63dde0
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2020-12-23 00:00:02 -0500

    gen doc

--- script/gen-doc.sh
+++ script/gen-doc.sh
@@ -8,7 +8,7 @@ for d in typed/* ; do
   echo "$d"
   if [ -d "$d" ]; then
     cd $d
-    #./script/gen-doc.sh
+    ./script/gen-doc.sh
     cd ../..
     cp -r "$d/target/codox" "target/codox/typed.$(basename $d)"
   fi
