bda59914211a24cedb91f79daf3f2ae8ecec3335
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2023-09-19 01:21:46 -0500

    tests

--- typed/clj.checker/test/clojure/core/typed/test/assoc.clj
+++ typed/clj.checker/test/clojure/core/typed/test/assoc.clj
@@ -1,6 +1,87 @@
 (ns clojure.core.typed.test.assoc
-  (:require [typed.clj.checker.test-utils :refer :all]
+  (:require [clojure.core.typed :refer [check-ns]]
+            [typed.clj.checker.test-utils :refer :all]
+            [typed.cljc.checker.type-rep :refer :all :as r]
+            [typed.cljc.checker.filter-ops :refer :all]
+            [typed.cljc.checker.object-rep :refer :all]
+            [typed.cljc.checker.type-ctors :refer :all]
             [clojure.test :refer :all]))
+
+(deftest invoke-assoc-test
+  
+  ; HMaps
+  (equal-types (assoc {} :a 5)
+               (HMap :mandatory {:a '5} :complete? true))
+  
+  (equal-types (assoc nil :a 5)
+               (HMap :mandatory {:a '5} :complete? true))
+  
+  (equal-types (assoc (clojure.core.typed/ann-form nil (t/U nil (HMap))) :a 5)
+               (HMap :mandatory {:a '5}))
+  
+  (equal-types (assoc (clojure.core.typed/ann-form nil (t/U nil (t/HMap :complete? true))) :a 5)
+               (HMap :mandatory {:a '5} :complete? true))
+  
+  (equal-types (assoc (clojure.core.typed/ann-form {} (t/HMap)) :a 5)
+               (HMap :mandatory {:a '5}))
+  
+  (equal-types (assoc (clojure.core.typed/ann-form {} (t/HMap :optional {:a clojure.core.typed/Any})) :b "v")
+               (HMap :mandatory {:b (Value "v")} :optional {:a Any}))
+  
+  (equal-types (assoc (clojure.core.typed/ann-form {} (t/HMap :optional {:a clojure.core.typed/Any})) :a "v")
+               (HMap :mandatory {:a (Value "v")}))
+
+  ;CTYP-79 resolve types properly in assoc
+  (is (check-ns 'clojure.core.typed.test.hmap-resolve-assoc))
+  
+  ; HVecs
+  (equal-types-noparse (assoc [] 0 1)
+                       (-hvec [(-val 1)]
+                              :filters [(-true-filter)]
+                              :objects [-empty]))
+  
+  (equal-types-noparse (assoc [3] 1 2)
+                       (-hvec [(-val 3) (-val 2)]
+                              :filters [(-true-filter)
+                                        (-true-filter)]
+                              :objects [-empty -empty]))
+  
+  (equal-types-noparse (assoc [0] 0 1)
+                       (-hvec [(-val 1)]
+                              :filters [(-true-filter)]
+                              :objects [-empty]))
+  
+  (equal-types-noparse (assoc [0] 0 (if (clojure.core.typed/ann-form 1 clojure.core.typed/Any) 1 2))
+                       (-hvec [(Un (-val 1) (-val 2))]
+                              :filters [(-true-filter)]
+                              :objects [-empty]))
+  
+  ; Basic types
+  (is-tc-e (assoc {} 'a 5)
+           (t/Map 'a '5))
+  
+  (is-tc-e (assoc {:b 6} 'a 5)
+           (t/Map (t/U 'a ':b) (t/U '5 '6)))
+  (is-tc-err (assoc {:b 6} 'a 5)
+             (t/Map ':b (t/U '5 '6)))
+  (is-tc-err (assoc {:b 6} 'a 5)
+             (t/Map (t/U 'a ':b) '5))
+  
+  (is-tc-e (fn [m :- (t/U nil (t/Map t/Any t/Any))]
+             :- (t/Map t/Any t/Any)
+             (assoc m :a 5)))
+  
+  (is-tc-e (fn [m :- (t/Map t/Any t/Any)]
+             :- (t/Map t/Any t/Any)
+             (assoc m :a 5)))
+  
+  (is-tc-e (fn [m :- (t/Vec t/Any)] 
+             :- (t/Vec t/Any)
+             (assoc m 0 2)))
+  
+  ;; TODO: assocs on records
+  
+  )
 
 (deftest assoc-test
   (is-tc-e (assoc {:a 1} :b 2)
