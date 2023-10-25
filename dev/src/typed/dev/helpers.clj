ab2bb4ef3438ca7c7215bc4c7d6b09a78034bed3
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2023-10-24 23:45:45 -0500

    clojure 1.12.0-alpha5

--- dev/src/typed/dev/helpers.clj
+++ dev/src/typed/dev/helpers.clj
@@ -14,7 +14,7 @@
       str/trim))
 
 (def selmer-input-map
-  {:clojure-mvn-version "1.12.0-alpha4"
+  {:clojure-mvn-version "1.12.0-alpha5"
    :clojars-url "https://clojars.org/repo"
    :sonatype-oss-public-url "https://oss.sonatype.org/content/groups/public/"
    :clojurescript-mvn-version "1.11.60"
