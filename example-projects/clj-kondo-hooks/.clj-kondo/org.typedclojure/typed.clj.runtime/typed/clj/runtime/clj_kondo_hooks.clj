67eb7bc9aa4d46446e162f0a7221143bae892db2
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2023-11-04 18:53:14 -0500

    rm temp files

--- example-projects/clj-kondo-hooks/.clj-kondo/org.typedclojure/typed.clj.runtime/typed/clj/runtime/clj_kondo_hooks.clj
+++
@@ -1,7 +0,0 @@
-(ns typed.clj.runtime.clj-kondo-hooks
-  (:require [clj-kondo.hooks-api :as kondo]))
-
-(defn _ann-form
-  [{:keys [node]}]
-  (let [[body _type] (rest (:children node))]
-    {:node body}))
