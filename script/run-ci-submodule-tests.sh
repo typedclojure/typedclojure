7ef50ae3db663a713960a06b804ca0927da71a92
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2023-11-23 15:50:20 -0500

    fix

--- script/run-ci-submodule-tests.sh
+++ script/run-ci-submodule-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+set -ex
 SUBMODULE_ARRAY=($SUBMODULE)
 # space separated list of submodules
 # https://stackoverflow.com/a/30212526
@@ -9,15 +9,19 @@ if [[ ${#SUBMODULE_ARRAY[@]} == 0 ]]; then
   echo "Empty submodule array!"
   exit 1
 fi
+if [[ -z $CLOJURE_VERSION ]]; then
+  echo 'Must set $CLOJURE_VERSION'
+  exit 1
+fi
 # threads seem to race to create this if it doesn't exist
 mkdir -p ~/.config/clojure
 echo "{}" > ~/.config/clojure/deps.edn
 download_deps (){
-  ./script/test -P -Sdeps '{:deps {org.clojure/clojure {:mvn/version "${{ matrix.clojure }}"}} :mvn/repos {"sonatype-oss-public" {:url "https://oss.sonatype.org/content/groups/public/"}}}'
+  ./script/test -P -Sdeps '{:deps {org.clojure/clojure {:mvn/version "'$CLOJURE_VERSION'"}} :mvn/repos {"sonatype-oss-public" {:url "https://oss.sonatype.org/content/groups/public/"}}}'
 }
 export -f download_deps
 run_tests (){
-  ./script/test -Sdeps '{:deps {org.clojure/clojure {:mvn/version "${{ matrix.clojure }}"}} :mvn/repos {"sonatype-oss-public" {:url "https://oss.sonatype.org/content/groups/public/"}}}'
+  ./script/test -Sdeps '{:deps {org.clojure/clojure {:mvn/version "'$CLOJURE_VERSION'"}} :mvn/repos {"sonatype-oss-public" {:url "https://oss.sonatype.org/content/groups/public/"}}}'
 }
 export -f run_tests
 if [[ "${SKIP_DOWNLOAD}" != "true" ]]; then
