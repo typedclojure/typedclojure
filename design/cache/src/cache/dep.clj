fc89c4cf3396882d62fa3c8f1630442f29aae113
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-05-20 20:21:22 -0500

    [skip ci]

--- design/cache/src/cache/dep.clj
+++ design/cache/src/cache/dep.clj
@@ -1,12 +1,17 @@
 (ns cache.dep
   (:require [cache.dep1 :as dep1]
-            [clojure.core.typed :as t]
-            [typed.clojure :as tc]))
+            [typed.clojure :as t]))
 
 (t/ann foo [:-> t/Any])
 (defn foo [] (inc (dep1/a)))
 
 (comment
-  (t/check-ns *ns*)
-  (tc/check-ns-clj *ns* :check-config {:check-ns-dep :recheck})
+  (t/check-ns-clj *ns* :check-config {:check-ns-dep :recheck})
+  ;Not checking typed.clojure (tagged with :typed.clojure/ignore metadata)
+  ;Not checking clojure.core.typed (tagged with :typed.clojure/ignore metadata)
+  ;Start checking cache.dep1
+  ;Checked cache.dep1 in 49.832031 msecs
+  ;Start checking cache.dep
+  ;Checked cache.dep in 56.33667 msecs
+  :ok
   )
