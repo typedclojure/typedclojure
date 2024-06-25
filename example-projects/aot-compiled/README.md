61a95a1b9d98c32515501c4104593a5d5bb629ff
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-06-25 17:12:51 -0500

    add aot test

---
+++ example-projects/aot-compiled/README.md
@@ -0,0 +1,3 @@
+# AOT compilation
+
+This project tests that `t/pred` forms can be AOT compiled.
---
+++ example-projects/aot-compiled/test/typed_example/aot_compiled_test.clj
@@ -0,0 +1,6 @@
+(ns typed-example.aot-compiled-test
+  (:require [clojure.test :refer [deftest is]]))
+
+(deftest aot-compiled-test
+  (compile 'typed-example.aot-compiled)
+ )
