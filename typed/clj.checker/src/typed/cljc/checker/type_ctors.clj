cade8719d3ebe720b534d0ac4e9d8d9a00750456
Oleksandr Yakushev <alex@bytopia.org>
2024-06-30 21:18:48 +0300

    Merge reduce2 and reduce4

--- typed/clj.checker/src/typed/cljc/checker/type_ctors.clj
+++ typed/clj.checker/src/typed/cljc/checker/type_ctors.clj
@@ -23,7 +23,7 @@
             [clojure.reflect :as reflect]
             [clojure.repl :as repl]
             [clojure.set :as set]
-            [typed.cljc.runtime.perf-utils :refer [repeatedly reduce2 reduce4]]
+            [typed.cljc.runtime.perf-utils :as perf :refer [repeatedly]]
             [typed.clj.checker.rclass-env :as rcls]
             [typed.cljc.checker.cs-rep :as crep]
             [typed.cljc.checker.datatype-env :as dtenv]
@@ -1427,12 +1427,12 @@
   {:pre [(= (count vs)
             (count ts))]}
   (persistent!
-   (reduce2 (fn [acc v t]
-              {:pre [(symbol? v)
-                     (r/Type? t)]}
-              (assoc! acc v (crep/t-subst-maker t r/no-bounds)))
-            (transient {})
-            vs ts)))
+   (perf/reduce (fn [acc v t]
+                  {:pre [(symbol? v)
+                         (r/Type? t)]}
+                  (assoc! acc v (crep/t-subst-maker t r/no-bounds)))
+                (transient {})
+                vs ts)))
 
 (t/ann ^:no-check instantiate-typefn [TypeFn (t/Seqable r/Type) t/Any t/Any -> r/Type])
 (defn instantiate-typefn [t types {:keys [names tapp]
@@ -1456,7 +1456,7 @@
           body (TypeFn-body* names bbnds t opts)
           opts (-> opts (free-ops/with-bounded-frees (zipmap (map r/make-F names) bbnds)))
           ;;check bounds
-          _ (reduce4
+          _ (perf/reduce
               (fn [_ argn nm type bnd]
                 {:pre [(r/Type? type)]}
                 (when-not (ind/has-kind? type bnd opts)
--- typed/clj.runtime/src/typed/cljc/runtime/perf_utils.clj
+++ typed/clj.runtime/src/typed/cljc/runtime/perf_utils.clj
@@ -1,5 +1,5 @@
 (ns ^:no-doc typed.cljc.runtime.perf-utils
-  (:refer-clojure :exclude [every? some repeatedly]))
+  (:refer-clojure :exclude [every? some repeatedly reduce]))
 
 (defn some
   "Like `clojure.core/some`, but uses an iterator over `lst`."
@@ -29,36 +29,46 @@
       (aset arr i (f)))
     (vec arr)))
 
-(defn reduce2
-  "Like `clojure.core/reduce`, but goes over two collections at once and accepts a
-  3-arity `f`. Uses iterators to iterate the collections."
-  [f init coll1 coll2]
-  (if (or (nil? coll1) (nil? coll2))
-    init
-    (let [it1 (.iterator ^Iterable coll1)
-          it2 (.iterator ^Iterable coll2)]
-      (loop [res init]
-        (if (and (.hasNext it1) (.hasNext it2))
-          (let [res (f res (.next it1) (.next it2))]
-            (if (reduced? res)
-              @res
-              (recur res)))
-          res)))))
-
-(defn reduce4
-  "Like `clojure.core/reduce`, but goes over four collections at once and accepts
-  a 5-arity `f`. Uses iterators to iterate the collections."
-  [f init coll1 coll2 coll3 coll4]
-  (if (or (nil? coll1) (nil? coll2) (nil? coll3) (nil? coll4))
-    init
-    (let [it1 (.iterator ^Iterable coll1)
-          it2 (.iterator ^Iterable coll2)
-          it3 (.iterator ^Iterable coll3)
-          it4 (.iterator ^Iterable coll4)]
-      (loop [res init]
-        (if (and (.hasNext it1) (.hasNext it2) (.hasNext it3) (.hasNext it4))
-          (let [res (f res (.next it1) (.next it2) (.next it3) (.next it4))]
-            (if (reduced? res)
-              @res
-              (recur res)))
-          res)))))
+(defn reduce
+  "Like `clojure.core/reduce`, but goes over several collections at once. The
+  provided `f` should accept the same number of arguments as N of collections +
+  1. Uses iterators to iterate the collections."
+  ([f init coll1 coll2]
+   (if (or (nil? coll1) (nil? coll2))
+     init
+     (let [it1 (.iterator ^Iterable coll1)
+           it2 (.iterator ^Iterable coll2)]
+       (loop [res init]
+         (if (and (.hasNext it1) (.hasNext it2))
+           (let [res (f res (.next it1) (.next it2))]
+             (if (reduced? res)
+               @res
+               (recur res)))
+           res)))))
+  ([f init coll1 coll2 coll3]
+   (if (or (nil? coll1) (nil? coll2) (nil? coll3))
+     init
+     (let [it1 (.iterator ^Iterable coll1)
+           it2 (.iterator ^Iterable coll2)
+           it3 (.iterator ^Iterable coll3)]
+       (loop [res init]
+         (if (and (.hasNext it1) (.hasNext it2) (.hasNext it3))
+           (let [res (f res (.next it1) (.next it2) (.next it3))]
+             (if (reduced? res)
+               @res
+               (recur res)))
+           res)))))
+  ([f init coll1 coll2 coll3 coll4]
+   (if (or (nil? coll1) (nil? coll2) (nil? coll3) (nil? coll4))
+     init
+     (let [it1 (.iterator ^Iterable coll1)
+           it2 (.iterator ^Iterable coll2)
+           it3 (.iterator ^Iterable coll3)
+           it4 (.iterator ^Iterable coll4)]
+       (loop [res init]
+         (if (and (.hasNext it1) (.hasNext it2) (.hasNext it3) (.hasNext it4))
+           (let [res (f res (.next it1) (.next it2) (.next it3) (.next it4))]
+             (if (reduced? res)
+               @res
+               (recur res)))
+           res))))))
