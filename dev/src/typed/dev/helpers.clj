023cfdccf25440177aae73b19d64e98bb769a1f4
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2025-03-08 23:02:19 -0600

    rm tools.nrepl

--- dev/src/typed/dev/helpers.clj
+++ dev/src/typed/dev/helpers.clj
@@ -39,8 +39,6 @@
    :tools-reader-mvn-version "1.5.0"
    :core-cache-mvn-version "1.1.234"
    :core-async-mvn-version "1.7.701"
-   ;; FIXME clj.checker fails on higher
-   :tools-nrepl-mvn-version "0.2.6"
    :test-check-mvn-version "1.1.1"
    :test-chuck-mvn-version "0.2.12"
    :core-logic-mvn-version "1.1.0"
