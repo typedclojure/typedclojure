ad0429a7ffeffd77ed377274cb07af93f5319ded
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2025-10-16 15:33:15 -0500

    license

--- build.clj
+++ build.clj
@@ -20,6 +20,9 @@
                 :version version
                 :basis basis
                 :packaging "jar"
+                :licenses [{:name "Eclipse Public License 1.0"
+                            :url "https://opensource.org/licenses/epl-1-0"
+                            :distribution "repo"}]
                 :scm {:url (:typedclojure-git-https-url h/selmer-input-map)
                       :connection (:typedclojure-scm-connection h/selmer-input-map)
                       :developerConnection (:typedclojure-scm-developerConnection h/selmer-input-map)
