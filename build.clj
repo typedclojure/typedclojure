098d1060643329eea8ab0f3b0de9dce81de72f5b
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2026-05-13 22:50:12 -0500

    rm typed.java.analyzer

--- build.clj
+++ build.clj
@@ -77,8 +77,7 @@
 
 ;;TODO topological sort
 (def sorted-modules
-  '[org.typedclojure/typed.java.analyzer
-    org.typedclojure/typed.cljc.analyzer
+  '[org.typedclojure/typed.cljc.analyzer
     org.typedclojure/typed.clj.analyzer
     org.typedclojure/typed.lib.spec.alpha
     org.typedclojure/typed.lib.clojure
--- typed/java.analyzer/test/typed_test/java/analyzer.clj
+++
@@ -1,1 +0,0 @@
-(ns typed-test.java.analyzer)
