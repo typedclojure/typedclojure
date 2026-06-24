da148d221045b4d0d5d40ae0bcdd8c0f1a0bf856
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2026-06-24 16:43:15 -0700

    widen singleton Long and Double loop inits to their class

--- typed/clj.checker/test/typed_test/cljc/checker/check/let.clj
+++ typed/clj.checker/test/typed_test/cljc/checker/check/let.clj
@@ -5,6 +5,9 @@
 (deftest core-loop-unannotated-test
   (is-tc-e #(cc/loop [a 1] (t/ann-form a Long)))
   (is-tc-err #(cc/loop [a 1] (recur (t/ann-form 1 t/Int))))
-  ;; TODO generalize init type
-  (do (is-tc-err #(cc/loop [a 1] (recur 2)))
-      (is-tc-e #(cc/loop [a 1] (t/ann-form a '1)))))
+  (is-tc-e #(cc/loop [a 1] (recur 2)))
+  (is (= '{:expected-type (t/Val 1), :actual-type Long}
+         (-> (is-tc-err #(cc/loop [a 1] (t/ann-form a '1)))
+             :type-errors
+             first
+             :data))))
